5087624e8bed57f09bd8e9e3bca2c55a8c47bd41
unknown <unabiazander@gmail.com>
2024-06-07 17:12:55 +0800

    implemented tasks for bizkit interview

--- phasebook/match.py
+++ phasebook/match.py
@@ -6,22 +6,20 @@ from .data.match_data import MATCHES
 
 bp = Blueprint("match", __name__, url_prefix="/match")
 
+# Preprocess the match data, converting lists to sets
+converted_matches = [(set(fave_numbers_1), set(fave_numbers_2)) for fave_numbers_1, fave_numbers_2 in MATCHES]
 
 @bp.route("<int:match_id>")
 def match(match_id):
-    if match_id < 0 or match_id >= len(MATCHES):
+    if match_id < 0 or match_id >= len(converted_matches):
         return "Invalid match id", 404
 
     start = time.time()
-    msg = "Match found" if (is_match(*MATCHES[match_id])) else "No match"
+    msg = "Match found" if (is_match(*converted_matches[match_id])) else "No match"
     end = time.time()
 
     return {"message": msg, "elapsedTime": end - start}, 200
 
-
+# Check if there are  any matches
 def is_match(fave_numbers_1, fave_numbers_2):
-    for number in fave_numbers_2:
-        if number not in fave_numbers_1:
-            return False
-
-    return True
+    return fave_numbers_2.issubset(fave_numbers_1)
--- phasebook/search.py
+++ phasebook/search.py
@@ -10,10 +10,8 @@ bp = Blueprint("search", __name__, url_prefix="/search")
 def search():
     return search_users(request.args.to_dict()), 200
 
-
 def search_users(args):
     """Search users database
-
     Parameters:
         args: a dictionary containing the following search parameters:
             id: string
@@ -24,7 +22,33 @@ def search_users(args):
     Returns:
         a list of users that match the search parameters
     """
-
     # Implement search here!
-
-    return USERS
+    results = []
+    for user in USERS:
+        matches = []
+        # Check ID if it matches
+        if "id" in args and user["id"] == args["id"]:
+            matches.append("id")
+        # Check name
+        if "name" in args and args["name"].lower() in user["name"].lower():
+            matches.append("name")
+        # Check age range
+        if "age" in args:
+            try:
+                age = int(args["age"])
+                if age - 1 <= user["age"] <= age + 1:
+                    matches.append("age")
+            except ValueError:
+                pass  # Ignore invalid age input
+        # Check occupation
+        if "occupation" in args and args["occupation"].lower() in user["occupation"].lower():
+            matches.append("occupation")
+        if matches:
+            results.append((user, matches))
+    # return the search results sorted based on the following priority on how they were matched
+    return [user for user, _ in sorted(results, key=lambda x: [
+        "id" in x[1], 
+        "name" in x[1], 
+        "age" in x[1], 
+        "occupation" in x[1]
+    ], reverse=True)]
